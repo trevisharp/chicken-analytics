9b53a772ff2f858862c26479d221bfee7ed1f6e6
trevisharp <leonardotrevisansilio@gmail.com>
2026-04-27 01:17:20 -0300

    improve spark config and lib instalations using dockerfile

--- docker/pipelines/silver_transform_pipeline.py
+++ docker/pipelines/silver_transform_pipeline.py
@@ -1,6 +1,10 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.getOrCreate()
+spark = SparkSession.builder.appName("silver") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.2.0") \
+    .getOrCreate()
 
 df = spark.read.json("data/bronze/")
 
